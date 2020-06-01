9fc133be2f8bd4cb9f7bc93abd47c725558ef96b
Felix Loehring <loehfelix@gmail.com>
2020-06-01 14:13:05 +0200

    add step 4

--- part1/courseinfo/src/index.js
+++ part1/courseinfo/src/index.js
@@ -22,7 +22,7 @@ const Content = (props) => {
 const Total = (props) => {
     return (
         <>
-            <p>Number of exercises {props.exercises.reduce((a,b) => a + b)}</p>
+            <p>Number of exercises {props.exercises.reduce((a, b) => a + b)}</p>
         </>
     )
 }
@@ -39,24 +39,26 @@ const Part = (props) => {
 
 const App = () => {
     const course = 'Half Stack application development'
-    const part1 = {
-        name: 'Fundamentals of React',
-        exercises: 10
-    }
-    const part2 = {
-        name: 'Using props to pass data',
-        exercises: 7
-    }
-    const part3 = {
-        name: 'State of a component',
-        exercises: 14
-    }
+    const parts = [
+        {
+            name: 'Fundamentals of React',
+            exercises: 10
+        },
+        {
+            name: 'Using props to pass data',
+            exercises: 7
+        },
+        {
+            name: 'State of a component',
+            exercises: 14
+        }
+    ]
 
     return (
         <div>
             <Header course={course}/>
-            <Content content={[part1, part2, part3]}/>
-            <Total exercises={[part1, part2, part3].map((part) => part.exercises)}/>
+            <Content content={parts}/>
+            <Total exercises={parts.map((part) => part.exercises)}/>
         </div>
     )
 }
