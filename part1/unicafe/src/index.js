315dcfe7c5fdd2f3746c7ee485a3ef26cd76aa77
Felix Loehring <loehfelix@gmail.com>
2020-06-05 19:53:02 +0200

    add step2

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -21,6 +21,8 @@ const App = () => {
     const [neutral, setNeutral] = useState(0)
     const [bad, setBad] = useState(0)
 
+    const all = bad + good + neutral
+    console.log(all)
 
     return (
         <div>
@@ -28,10 +30,16 @@ const App = () => {
             <button onClick={() => setGood(good + 1)}>good</button>
             <button onClick={() => setNeutral(neutral + 1)}>neutral</button>
             <button onClick={() => setBad(bad + 1)}>bad</button>
-            <h1>statistics</h1>
-            <p>good {good}</p>
-            <p>neutral {neutral}</p>
-            <p>bad {bad}</p>
+            <div>
+                <h1>statistics</h1>
+                good {good} <br/>
+                neutral {neutral} <br/>
+                bad {bad} <br/>
+                all {all} <br/>
+                average {all / 3} <br/>
+                positive {good / all * 100}%
+            </div>
+
         </div>
     )
 }
