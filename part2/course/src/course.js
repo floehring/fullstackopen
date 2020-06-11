b5da84e502efa49c51b97507013c44bc7939b0ce
Felix Loehring <loehfelix@gmail.com>
2020-06-11 13:59:50 +0200

    add step7

--- part2/course/src/course.js
+++ part2/course/src/course.js
@@ -1,46 +1,43 @@
 import React from "react";
 
 const Header = ({course}) => {
-    return (
-        <h1>{course.name}</h1>
-    )
+    return <h1>{course.name}</h1>
 }
 
-// const Total = ({course}) => {
-//     const sum = course.parts[0].exercises + course.parts[1].exercises + course.parts[2].exercises
-//     return (
-//         <p>Number of exercises {sum}</p>
-//     )
-// }
+const Total = ({course}) => {
+    console.log('Total content:', course)
+
+    const sum = course.parts
+        .map((part) => part.exercises)
+        .reduce((x, y) => x + y)
+
+    return <p><strong>total of {sum} exercises </strong></p>
+}
 
 const Part = ({content}) => {
-    console.log(content)
-    return (
-        <p>
-            {content.name} {content.exercises}
-        </p>
-    )
+    console.log('Part content:', content)
+    return <p> {content.name} {content.exercises} </p>
 }
 
 const Content = ({course}) => {
-    console.log('course:', course)
-    return (course.parts.map(part => {
-            console.log('part', part)
+    console.log('Content course:', course)
+    return course.parts.map(part => {
             return <Part key={part.id} content={part}/>;
         }
-    ))
+    )
 }
 
 const Course = ({content}) => {
-    return (<
-        div>
-        < Header
-            course={content}
-        />
-        <Content course={content}/>
-        {/*<Total course={course}/>*/
-        }
-    </div>)
+    console.log(content)
+    return (
+        <div>
+            < Header
+                course={content}
+            />
+            <Content course={content}/>
+            <Total course={content}/>
+
+        </div>)
 }
 
 export default Course
