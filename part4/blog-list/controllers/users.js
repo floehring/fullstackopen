a0bc29a7a59467618bc99e0e84075669b400f902
Felix Loehring <loehfelix@gmail.com>
2020-11-20 16:06:00 +0100

    add error codes and password length verification

--- part4/blog-list/controllers/users.js
+++ part4/blog-list/controllers/users.js
@@ -5,6 +5,12 @@ const User = require('../models/user')
 usersRouter.post('/', async (request, response) => {
     const body = request.body
 
+    if (!body.password || body.password.length < 3) {
+        return response.status(400).json({
+            error: 'password must be at least 3 characters long'
+        })
+    }
+
     const saltRounds = 10
     const passwordHash = await bcrypt.hash(body.password, saltRounds)
 
@@ -14,9 +20,16 @@ usersRouter.post('/', async (request, response) => {
         passwordHash,
     })
 
-    const savedUser = await user.save()
+    try {
+        const savedUser = await user.save()
+        return response.json(savedUser)
+    } catch (e) {
+        response.status(400).json({
+            error: e.message
+        })
+    }
 
-    response.json(savedUser)
+
 })
 
 module.exports = usersRouter
