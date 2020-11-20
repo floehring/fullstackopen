526ffd8d5fece872617fd4f51456aaaaa775f76b
Felix Loehring <loehfelix@gmail.com>
2020-11-20 16:06:58 +0100

    add tests for users

---
+++ part4/blog-list/tests/users.test.js
@@ -0,0 +1,44 @@
+const helper = require('./test_helper')
+const mongoose = require('mongoose')
+const supertest = require('supertest')
+const app = require('../app')
+const api = supertest(app)
+
+const bcrypt = require('bcrypt')
+const User = require('../models/user')
+
+describe('when there is initially one user in db', () => {
+    beforeEach(async () => {
+        await User.deleteMany({})
+
+        const passwordHash = await bcrypt.hash('secret', 10)
+        const user = new User({ username: 'root', passwordHash })
+
+        await user.save()
+    })
+
+    test('creation succeeds with fresh username', async () => {
+        const usersAtStart = await helper.usersInDb()
+        const newUser = {
+            username: 'clearck',
+            name: 'Felix Loehring',
+            password: 'mypw123'
+        }
+
+        await api
+            .post('/api/users')
+            .send(newUser)
+            .expect(200)
+            .expect('Content-Type', /application\/json/)
+
+        const usersAtEnd = await helper.usersInDb()
+        expect(usersAtEnd).toHaveLength(usersAtStart.length + 1)
+
+        const usernames = usersAtEnd.map(user => user.username)
+        expect(usernames).toContain(newUser.username)
+    })
+})
+
+afterAll(() => {
+    mongoose.connection.close()
+})
