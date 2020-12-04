19997a125d08b6b0442f50f72befa44931a0f36a
Felix Loehring <loehfelix@gmail.com>
2020-12-04 15:53:05 +0100

    changed position of logout button

--- part5/bloglist-frontend-master/src/App.js
+++ part5/bloglist-frontend-master/src/App.js
@@ -84,7 +84,7 @@ const App = () => {
         </div>
         : <div>
           <h2>blogs</h2>
-          <p>{user.name} logged in</p>
+          {user.name} logged in
           <button onClick={() => window.localStorage.removeItem('loggedBloglistUser')}>logout</button>
           {blogs.map(blog =>
             <Blog key={blog.id} blog={blog}/>
